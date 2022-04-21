d8f80c530998e74c670d1b732441de72d74802d0
Viktor Denikin <leshden@yandex.ru>
2022-04-21 21:08:01 +0300

    sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
+import './categories.styles.scss';
+import CategoryItem from './components/category-item/component';
 
 function App() {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Маски',
+      imageUrl: 'https://i.ibb.co/58rtkM1/mask.jpg',
+    },
+
+    {
+      id: 2,
+      title: 'Костюмы',
+      imageUrl: 'https://i.ibb.co/yY0g6VK/suit.jpg',
+    },
+
+    {
+      id: 3,
+      title: 'Обувь',
+      imageUrl: 'https://i.ibb.co/CmFmQSq/shoes.jpg',
+    },
+
+    {
+      id: 4,
+      title: 'Дамы',
+      imageUrl: 'https://i.ibb.co/jWKpSWx/women.jpg',
+    },
+
+    {
+      id: 5,
+      title: 'Мужчины',
+      imageUrl: 'https://i.ibb.co/NsZvwJG/men.jpg',
+    },
+
+  ];
+
   return (
-    <div>
-      Batman-Shop
+    <div className='categories-container'>
+      {categories.map((category)=>(
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
 }
---
+++ src/components/category-item/component.jsx
@@ -0,0 +1,19 @@
+import './styles.scss';
+
+const CategoryItem = ({category}) => {
+  const {imageUrl, title} = category;
+  return(
+    <div className='category-container'>
+      <div className='background-image'
+       style={{
+        backgroundImage: `url(${imageUrl})`,
+      }} />
+      <div className='category-body-container'>
+        <h2>{title}</h2>
+        <p>Купить</p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
