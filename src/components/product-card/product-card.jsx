eb56f416468fcdc55fe83fa4a4fd89a52a482948
Viktor Denikin <leshden@yandex.ru>
2022-04-28 18:03:23 +0300

    product-card

--- src/components/product-card/product-card.jsx
+++ src/components/product-card/product-card.jsx
@@ -1,7 +1,18 @@
-import '/product-card.scss';
+import './product-card.scss';
+import Button from '../button/button';
 
-function ProductCard() {
-
+function ProductCard({product}) {
+  const {name, price, imageUrl} = product;
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={name}/>
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted'>Добавить</Button>
+    </div>
+  );
 }
 
 export default ProductCard;
